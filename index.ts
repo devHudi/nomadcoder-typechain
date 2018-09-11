e838cfb6f7c8183f492c9057a623e98fbc6a78f3
L0GIC <cdhnet98@gmail.com>
2018-09-11 11:47:05 +0900

    선택적 파라미터

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log("Hello world");
+const name = "Cho donghyun",
+  age = 21,
+  gender = "male";
+
+//파라미터 뒤에 ?을 붙이면 선택적 파라미터가 된다.
+//선택적 파라미터가 아닌 파라미터를 생략하게 되면, 에러가 발생하는 것이 일반 자바스크립트와 차이점이다.
+const sayHi = (name, age, gender?) => {
+  console.log(`Hello ${name}, you are ${age} and a ${gender}`);
+};
+
+sayHi(name, age);
+
+export {};
