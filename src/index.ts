1f3e027f9753b9442786cfe6223054f03c480abb
L0GIC <cdhnet98@gmail.com>
2018-09-11 23:27:33 +0900

    파라미터 타입 설정

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ const name = "Cho donghyun",
   age = 21,
   gender = "male";
 
-//파라미터 뒤에 ?을 붙이면 선택적 파라미터가 된다.
-//선택적 파라미터가 아닌 파라미터를 생략하게 되면, 에러가 발생하는 것이 일반 자바스크립트와 차이점이다.
+//파라미터 뒤에 : 를 사용하여 타입을 설정할 수 있다.
+//함수 파라미터 선언부 뒤에 : 를 사용하여 반환타입을 설정할 수 있다.
 const sayHi = (name: string, age: number, gender: string): void => {
   console.log(`Hello ${name}, you are ${age} and a ${gender}`);
 };
