e95346fdf952e3b3bf6ac1dfbf640ffef12b6bdf
L0GIC <cdhnet98@gmail.com>
2018-09-14 00:29:11 +0900

    블럭체인 완성

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,111 @@
-interface Human {
-  name: string;
-  age: number;
-  gender: string;
+import * as CryptoJS from "crypto-js";
+
+class Block {
+  //static 을 사용해서, 인스턴스를 만들지 않고도 접근 가능한 함수.
+  //Block 의 여러 정보를 Hash 로 만들어주는 함수
+  static calculateBlockHash = (
+    index: number,
+    previousHash: string,
+    timestamp: number,
+    data: string
+  ): string =>
+    CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
+  //Block 의 구조가 유효한지 검증한다.
+  static validateStructure = (aBlock: Block): boolean =>
+    typeof aBlock.index === "number" &&
+    typeof aBlock.hash === "string" &&
+    typeof aBlock.previousHash === "string" &&
+    typeof aBlock.timestamp === "number" &&
+    typeof aBlock.data === "string";
+
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public timestamp: number;
+  public data: string;
+
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    timestamp: number,
+    data: string
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.timestamp = timestamp;
+    this.data = data;
+  }
 }
-//Typescript 에는 인터페이스 개념이 존재한다.
-//쉽게 설명하자면, 파라미터로 object 를 받아올때 property 들을 검증하는 스키마 같은 개념이다.
 
-const person = {
-  name: "Hudi",
-  age: 21,
-  gender: "male"
-};
+const genesisBlock: Block = new Block(0, "adfasdfasdf", "", 12335, "Hello");
+//블럭체인의 최초 블럭
+//블럭체인에 블럭을 잇기 위해서 previousBlock의 Hash 가 필요하기 때문에 임의로 아무거나 생성함
 
-//변수 Type 대신 Human 이라는 인터페이스로 파라미터 검증을 한다.
-const sayHi = (person: Human): void => {
-  console.log(
-    `Hello ${person.name}, you are ${person.age} and a ${person.gender}`
+let blockchain: Block[] = [genesisBlock];
+//블럭체인 배열
+
+const getBlockchain = (): Block[] => blockchain;
+
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
+
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
+
+const createNewBlock = (data: string): Block => {
+  const previousBlock: Block = getLatestBlock();
+  const newIndex: number = previousBlock.index + 1;
+  const newTimeStamp: number = getNewTimeStamp();
+  const newHash: string = Block.calculateBlockHash(
+    newIndex,
+    previousBlock.hash,
+    newTimeStamp,
+    data
   );
+  const newBlock: Block = new Block(
+    newIndex,
+    newHash,
+    previousBlock.hash,
+    newTimeStamp,
+    data
+  );
+  addBlock(newBlock);
+  return newBlock;
 };
 
-sayHi(person);
+const getHashforBlock = (aBlock: Block): string =>
+  Block.calculateBlockHash(
+    aBlock.index,
+    aBlock.previousHash,
+    aBlock.timestamp,
+    aBlock.data
+  );
+
+const isBlockValid = (candidateBlock: Block, previousBlock: Block): boolean => {
+  if (!Block.validateStructure(candidateBlock)) {
+    return false;
+  } else if (previousBlock.index + 1 !== candidateBlock.index) {
+    return false;
+  } else if (previousBlock.hash !== candidateBlock.previousHash) {
+    return false;
+  } else if (getHashforBlock(candidateBlock) !== candidateBlock.hash) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+const addBlock = (candidateBlock: Block): void => {
+  if (isBlockValid(candidateBlock, getLatestBlock())) {
+    blockchain.push(candidateBlock);
+  }
+};
+
+createNewBlock("second block");
+createNewBlock("third block");
+createNewBlock("fourth block");
+
+console.log(blockchain);
 
 export {};
